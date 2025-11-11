789873bf16b919fff362ffe739701ce19449d088
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-11-11 14:58:08 +0530

    UC5_PremiumRides

--- src/main/java/com/example/cabinvoice/Ride.java
+++ src/main/java/com/example/cabinvoice/Ride.java
@@ -4,11 +4,18 @@ public class Ride {
 	
 	public double distance;
 	public int time;
-	
+	public RideType rideType;
+
 	
 	public Ride(double distance, int time) {
-		this.distance = distance;
-		this.time = time;
-	}
+        this.distance = distance;
+        this.time = time;
+      
+    }
+	public Ride(double distance, int time, RideType rideType) {
+        this.distance = distance;
+        this.time = time;
+        this.rideType = rideType;
+    }
 
 }
